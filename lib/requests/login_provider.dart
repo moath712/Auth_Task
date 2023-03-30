d54da4acff087ce1c72b5e68b059a9f307bda124
moath712 <moathasfoura712@gmail.com>
2023-03-30 12:56:09 +0300

    retrofit
    login request updated with retrofit

--- lib/requests/login_provider.dart
+++ lib/requests/login_provider.dart
@@ -1,29 +1,72 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'login_provider.g.dart';
 
 final twoFactorTokenProvider = StateProvider<String>((ref) => '');
-
 final twoFactorProvider = FutureProvider<AuthService>((ref) async {
-  return AuthService();
+  return AuthService(Dio());
 });
 
+@RestApi(baseUrl: 'https://services.himam.com/api/auth')
+abstract class AuthServiceApi {
+  factory AuthServiceApi(Dio dio, {String baseUrl}) = _AuthServiceApi;
+
+  @POST('/login')
+  Future<LoginResponse> login(@Body() LoginRequest request);
+}
+
+class LoginRequest {
+  final String email;
+  final String password;
+  final int clientOrganizationId;
+
+  LoginRequest({
+    required this.email,
+    required this.password,
+    required this.clientOrganizationId,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'email': email,
+      'password': password,
+      'clientOrganizationId': clientOrganizationId,
+    };
+  }
+}
+
+class LoginResponse {
+  final String twoFactorToken;
+
+  LoginResponse({required this.twoFactorToken});
+
+  factory LoginResponse.fromJson(Map<String, dynamic> json) {
+    return LoginResponse(
+      twoFactorToken: json['twoFactorToken'],
+    );
+  }
+}
+
 class AuthService {
+  final AuthServiceApi _api;
+
+  AuthService(Dio dio) : _api = AuthServiceApi(dio);
+
   Future<bool> login(
       {required String email,
       required String password,
       required WidgetRef ref}) async {
     try {
-      final Dio dio = Dio();
-      final response =
-          await dio.post('https://services.himam.com/api/auth/login', data: {
-        'email': email,
-        'password': password,
-        'clientOrganizationId': 1,
-      });
+      final response = await _api.login(LoginRequest(
+        email: email,
+        password: password,
+        clientOrganizationId: 1,
+      ));
 
-      final responseData = response.data;
-      final token = responseData['twoFactorToken'];
+      final token = response.twoFactorToken;
 
       ref.read(twoFactorTokenProvider.notifier).state = token;
 
